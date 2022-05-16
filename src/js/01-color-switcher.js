0719d5c349abac75d4f80b60d5a40b9fa2a2bfa6
KuzkinaOlga <o.kuzkina@gmail.com>
2022-05-16 16:53:46 +0200

    hw js 9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const buttonStart = document.querySelector('button[data-start]')
+const buttonStop = document.querySelector('button[data-stop]')
 
+let timerId = null;
+buttonStop.setAttribute('disabled', true)
+
+function startBtnClick() {
+    buttonStart.setAttribute('disabled', true)
+    buttonStop.removeAttribute('disabled')
+    timerId = setInterval(() => {
+        const color = getRandomHexColor();
+        document.body.style.background = color;
+        console.log(color);
+    }, 1000)
+    
+}
+buttonStart.addEventListener('click', startBtnClick)
+function stopBtnClick() {
+    buttonStart.removeAttribute('disabled');
+    clearInterval(timerId);
+    buttonStop.setAttribute('disabled', true)
+}
+buttonStop.addEventListener('click', stopBtnClick)
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio'
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+    if (shouldResolve) {
+    resolve(`Fulfilled promise ${position} in ${delay}ms`)
   } else {
-    // Reject
+    reject(`Rejected promise ${position} in ${delay}ms`)
   }
+  }, delay)
+ })
+}
+
+const formRef = document.querySelector('.form')
+function handleFormSubmit(evente) {
+  evente.preventDefault();
+  const { delay, step, amount } = evente.currentTarget;
+  let delayPromise = Number(delay.value)
+  for (let i = 1; i <= amount.value; i += 1){
+    createPromise(i, delayPromise).then(onResolved).catch(onRejected)
+    delayPromise += Number(step.value)
+  }
+  evente.currentTarget.reset()
+}
+formRef.addEventListener('submit', handleFormSubmit)
+function onResolved(result) {
+ Notify.success(result)
+
+}
+function onRejected(result) {
+  Notify.failure(result)
 }
